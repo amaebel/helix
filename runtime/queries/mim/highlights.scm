606fa675874704977efeb995bafe7b1c3433a71b
Manuel Lata <contact@mlata.me>
2026-01-09 17:39:53 +0100

    update mim grammar

--- runtime/queries/mim/highlights.scm
+++ runtime/queries/mim/highlights.scm
@@ -1,21 +1,22 @@
 ; ——— Identifiers & Annexes
-(value_identifier) @variable
+((identifier) @type
+  (#match? @type "^[A-Z][_a-zA-Z]*"))
 ;
-(type_identifier) @type
+(identifier) @variable
 ;
 (annex
   "%" @namespace
   module: (identifier) @namespace)
 (annex
-  name: (value_identifier) @variable.builtin)
+  name: (identifier) @variable.builtin)
 (annex
-  name: (type_identifier) @type.builtin)
-(annex
-  name: (identifier) @namespace
-  subtag: (value_identifier) @variable.builtin)
+  name: ((identifier) @type.builtin (#match? @type.builtin "^[A-Z][_a-zA-Z]*")))
 (annex
   name: (identifier) @namespace
-  subtag: (type_identifier) @type.builtin)
+  subtag: (identifier) @variable.builtin)
+(annex
+  name: (identifier) @namespace
+  subtag: ((identifier) @type.builtin (#match? @type.builtin "^[A-Z][_a-zA-Z]*")))
 
 ; ——— Functions & Parameters ———
 ;
@@ -24,11 +25,16 @@
   (pattern (identifier) @variable.parameter)*
   (pattern
     (pattern .(identifier) @variable.parameter))*
-  (pattern (group (identifier) @variable.parameter))*)
-(function_type
-  (pattern (pattern (identifier) @variable.parameter)))
-(function_type
-  (pattern (group (identifier) @variable.parameter)))
+  (pattern (group (identifier) @variable.parameter))*
+  (battern (identifier) @variable.parameter)*
+  (battern
+    (battern .(identifier) @variable.parameter))*
+  (battern (group (identifier) @variable.parameter))*)
+(pi
+  (battern (battern (identifier) @variable.parameter))*
+  (implicit (battern (identifier) @variable.parameter))*)
+(pi
+  (battern (group (identifier) @variable.parameter))*)
 ;
 ; highlight identifiers used as functions
 (application . (identifier) @variable.function (_))
@@ -59,6 +65,8 @@
   ","
   ";"
   "."
+  "|"
+  "$"
 ] @punctuation.delimiter
 
 ; ——— Operators ———
@@ -70,6 +78,8 @@
   "@"
   "->"
   "→"
+  "∪"
+  "=>"
 ] @operator
 
 ; ——— Keywords ———
@@ -81,17 +91,26 @@
 ;
 [
   "let"
+  "rec"
+  "and"
   "axm"
 ] @keyword.storage.type
 ;
 [
+  "match"
+  "with"
+  "ret"
+] @keyword.control
+;
+[
+  "cfun"
+  "ccon"
   "lam"
   "con"
   "fun"
   "lm"
   "cn"
   "fn"
-  "Sigma"
 ] @keyword.function
 ;
 [
@@ -99,23 +118,20 @@
   "end"
   "extern"
 ] @keyword
+;
+[
+  "inj"
+] @keyword.operator
+;
+[
+  "rule"
+  "norm"
+  "when"
+] @keyword.directive
 
 ; ——— Types
 ;
-[
-  "Cn"
-  "Fn"
-  "Nat"
-  "Bool"
-  "Idx"
-  "Type"
-  "Univ"
-  "*"
-  "★"
-  "□"
-  "⊥" ".bot"
-  "⊤" ".top"
-] @type.builtin
+(primitive) @type.builtin
 ;
 ; highlight identifiers that appear where types or type level functions
 ; are expected (with up to eight parameters)
@@ -129,11 +145,11 @@
 (_ type: (application (application (application (application (application (application (application (identifier) @type (_)) (_)) (_)) (_)) (_)) (_)) (_)))
 (_ type: (application (application (application (application (application (application (application (application (identifier) @type (_)) (_)) (_)) (_)) (_)) (_)) (_)) (_)))
 
-"return" @keyword.control
-
 ; ——— Literals ———
 ;
 (int_literal) @constant.numeric
+;
+(float_literal) @constant.numeric
 ;
 (char_literal) @constant.character
 ;
@@ -143,6 +159,17 @@
   "tt"
   "ff"
 ] @constant.builtin
+;
+; special "keywords"
+((identifier) @keyword.control.return
+  (#eq? @keyword.control.return "return"))
+;
+((identifier) @keyword.control.return
+  (#eq? @keyword.control.return "yield"))
+;
+((identifier) @keyword.control.return
+  (#eq? @keyword.control.return "continue"))
+;
 
 ; ——— Comments ———
 ; doc comment markdown injection is handled in injections.scm
